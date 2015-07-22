182fee6ce63cca75ca66773d1c6fc98dfe315692
Dennis Kupec <denniskupec@gmail.com>
2015-07-21 21:34:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        LibreSSL Comic Sans
+// @namespace   denniskupec.com
+// @include     http://www.libressl.org/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.body.style.fontFamily = "'Comic Sans MS','Comic Sans'";
+
